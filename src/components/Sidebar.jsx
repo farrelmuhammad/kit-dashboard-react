caa694359eed176568eceff8c4c87a03c391e50b
Farrel Muhammad <farrelmuhammad432@gmail..com>
2023-10-12 16:53:57 +0700

    Update sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
     return (
         <>
             <aside className="w-72" aria-label="Sidebar">
-                <div className="overflow-y-auto py-4 px-3 bg-gray-100 shadow-lg shadow-gray-400/50 rounded-lg h-[480px]">
+                <div className="overflow-y-auto py-4 px-3 bg-gray-100 shadow-lg shadow-gray-400/50 rounded-lg max-h-screen">
                     <div className="text-center my-4">
                         <div className="flex flex-col items-center">
                             <img src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=871&q=80"
@@ -13,7 +13,6 @@ const Sidebar = () => {
                             <p className="text-gray-500 text-xl">Have a great day ahead. 👋</p>
                         </div>
                     </div>
-
 
                     <ul className="mb-1">
                         <li>
@@ -41,6 +40,16 @@ const Sidebar = () => {
                                 className="flex items-center px-3 py-4 text-base font-normal rounded-lg text-gray-500 hover:bg-orange-500 hover:text-white">
                                 <i className="fas fa-money-bills text-lg"></i>
                                 <span className="ml-3">Employee</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul className="mb-1">
+                        <li>
+                            <a href="" id="menu-4"
+                                className="flex items-center px-3 py-4 text-base font-normal rounded-lg text-gray-500 hover:bg-orange-500 hover:text-white">
+                                <i className="pl-1 fas fa-user text-lg"></i>
+                                <span className="ml-4">Profile</span>
                             </a>
                         </li>
                     </ul>
